a726c56178aa84713d877b6d2bd86499048b0c50
Yano <103043303+yano0@users.noreply.github.com>
2024-01-01 23:44:25 +0900

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,8 +5,8 @@ const posts = Object.values(postImportResult);
 
 export const get = () =>
   rss({
-    title: "Astro Theme Creek",
-    description: "A Theme for Astro",
+    title: "Gyoza Paradice",
+    description: "individucal blog",
     site: import.meta.env.SITE,
     items: import.meta.glob("./posts/**/*.md"),
   });
